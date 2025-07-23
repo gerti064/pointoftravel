f8369d3c4913f4a98f08e7961f62116fab7ae2a7
andet-art <4n637fejzuli@gmail.com>
2025-07-23 15:51:14 +0200

    ,

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -30,7 +30,7 @@ const Contact: React.FC = () => {
     const payload = { name, email, message };
 
     try {
-      const response = await fetch("http://46.101.211.140/api/bookings/add_messages.php", {
+      const response = await fetch("http://46.101.211.140/api/contact/add_messages.php", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
